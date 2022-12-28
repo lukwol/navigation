1d81158fe596f6bab94fec266188427331bd20a2
lukwol <wolanczyk.lukasz@gmail.com>
2022-12-28 17:25:41 +0100

    Rework screen and window routes as String type alias

--- examples/mvi-multi-window/src/jvmMain/kotlin/io/github/lukwol/examples/AppRoutes.kt
+++ examples/mvi-multi-window/src/jvmMain/kotlin/io/github/lukwol/examples/AppRoutes.kt
@@ -1,12 +1,9 @@
 package io.github.lukwol.examples
 
-import io.github.lukwol.screens.navigation.ScreenRoute
-import io.github.lukwol.windows.navigation.WindowRoute
-
 object AppRoutes {
-    val FirstWindowRoute = WindowRoute("first-window")
-    val SecondWindowRoute = WindowRoute("second-window")
+    const val FirstWindowRoute = "first-window"
+    const val SecondWindowRoute = "second-window"
 
-    val FirstScreenRoute = ScreenRoute("first-screen")
-    val SecondScreenRoute = ScreenRoute("second-screen")
+    const val FirstScreenRoute = "first-screen"
+    const val SecondScreenRoute = "second-screen"
 }
